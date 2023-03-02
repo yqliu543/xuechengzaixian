35de3753c316af52a8b77b86d5ca1056b40745bc
yqliu <3471966565@qq.com>
2023-03-02 13:56:36 +0800

    544:

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileProcessService.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileProcessService.java
@@ -15,4 +15,18 @@ public interface MediaFileProcessService {
      * @date 2022/9/14 14:49
      */
     public List<MediaProcess> getMediaProcessList(int shardIndex, int shardTotal, int count);
+
+
+    /**
+     * @description 保存任务结果
+     * @param taskId  任务id
+     * @param status 任务状态
+     * @param fileId  文件id
+     * @param url url
+     * @param errorMsg 错误信息
+     * @return void
+     * @author Mr.M
+     * @date 2022/10/15 11:29
+     */
+    void saveProcessFinishStatus(Long taskId,String status,String fileId,String url,String errorMsg);
 }
--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileProcessServiceImpl.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileProcessServiceImpl.java
@@ -1,11 +1,18 @@
 package com.xuecheng.media.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xuecheng.media.mapper.MediaFilesMapper;
+import com.xuecheng.media.mapper.MediaProcessHistoryMapper;
 import com.xuecheng.media.mapper.MediaProcessMapper;
+import com.xuecheng.media.model.po.MediaFiles;
 import com.xuecheng.media.model.po.MediaProcess;
+import com.xuecheng.media.model.po.MediaProcessHistory;
 import com.xuecheng.media.service.MediaFileProcessService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,11 +27,39 @@ public class MediaFileProcessServiceImpl implements MediaFileProcessService {
 
     @Autowired
     private MediaProcessMapper mediaProcessMapper;
+    @Autowired
+    private MediaFilesMapper mediaFilesMapper;
+    @Autowired
+    private MediaProcessHistoryMapper mediaProcessHistoryMapper;
 
     @Override
     public List<MediaProcess> getMediaProcessList(int shardIndex, int shardTotal, int count) {
         List<MediaProcess> mediaProcesses = mediaProcessMapper.selectListByShardIndex(shardTotal, shardIndex, count);
-
         return mediaProcesses;
+    }
+
+    @Override
+    @Transactional
+    public void saveProcessFinishStatus(Long taskId, String status, String fileId, String url, String errorMsg) {
+        MediaProcess mediaProcess = mediaProcessMapper.selectById(taskId);
+        if (mediaProcess==null){
+            log.debug("更新任务状态此任务为空{}");
+            return;
+        }
+        LambdaQueryWrapper<MediaProcess> queryWrapper = new LambdaQueryWrapper<MediaProcess>().eq(MediaProcess::getId, taskId);
+        if ("3".equals(status)) {
+            MediaProcess mediaProcess1 = new MediaProcess();
+            mediaProcess1.setStatus("3");//处理失败
+            mediaProcessMapper.update(mediaProcess1,queryWrapper);
+            return;
+        }
+        if ("3".equals(status)) {
+            mediaProcess.setStatus("2");
+            mediaProcessMapper.updateById(mediaProcess);
+        }
+        MediaProcessHistory mediaProcessHistory = new MediaProcessHistory();
+        BeanUtils.copyProperties(mediaProcess,mediaProcessHistory);
+        mediaProcessHistoryMapper.insert(mediaProcessHistory);
+        mediaProcessMapper.deleteById(taskId);
     }
 }
