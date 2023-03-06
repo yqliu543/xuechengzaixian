c47e5e5e78c50d8b3ed59693b549837e55bf7082
yqliu <3471966565@qq.com>
2023-03-06 09:31:11 +0800

    544:

--- xuecheng-plus-auth/src/main/java/com/xuecheng/AuthApplication.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/AuthApplication.java
@@ -2,8 +2,10 @@ package com.xuecheng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.xuecheng.ucenter.feignclient")
 public class AuthApplication {
 
     public static void main(String[] args) {
